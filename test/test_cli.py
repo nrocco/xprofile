c2cac06364af55356d901621e035e509b703095a
Antti Ruotsalainen <antti.ruotsalainen@qvantel.com>
2015-05-07 18:15:05 +0300

    fixed problem where you can't create profiles if there is already a profile in config file. This did not needed to be with same edid. Also added a test to check for this case so it won't happen in the future.

--- test/test_cli.py
+++ test/test_cli.py
@@ -3,11 +3,11 @@ import tempfile
 
 from mock import patch
 
-from xprofile.xrandr import Xrandr
 from xprofile.__main__ import main
 from distutils.spawn import find_executable
 
 xrandr_bin = find_executable('xrandr')
+
 
 @patch('xprofile.xrandr.Popen')
 def test_create_new_profile(Popen):
@@ -49,14 +49,30 @@ def test_create_existing_profile(Popen):
     Popen.return_value.communicate.return_value = (xrandr_stdout, None)
     Popen.return_value.wait.return_value = 0
 
-    retval = main(['--config', 'test/xprofilerc_example', 'create', 'laptop'])
+    retval = main(['--config', 'test/xprofilerc_both_example', 'create', 'laptop'])
 
     assert retval == 1
     assert Popen.called
     assert Popen.call_args[0][0] == [xrandr_bin, '--verbose']
 
+
+@patch('xprofile.xrandr.Popen')
+def test_create_new_profile_when_other_profiles_exists(Popen):
+    with open('test/docked.txt', 'rb') as file:
+        xrandr_stdout = file.read()
+
+    Popen.return_value.communicate.return_value = (xrandr_stdout, None)
+    Popen.return_value.wait.return_value = 0
+
+    retval = main(['--config', 'test/xprofilerc_docked_only_example', 'create', 'not_in_config'])
+
+    assert retval == 0
+    assert Popen.called
+    assert Popen.call_args[0][0] == [xrandr_bin, '--verbose']
+
+
 def test_list():
-    main(['--config', 'test/xprofilerc_example', 'list'])
+    main(['--config', 'test/xprofilerc_both_example', 'list'])
 
 
 @patch('xprofile.xrandr.Popen')
@@ -67,7 +83,7 @@ def test_current(Popen):
     Popen.return_value.communicate.return_value = (xrandr_stdout, None)
     Popen.return_value.wait.return_value = 0
 
-    retval = main(['--config', 'test/xprofilerc_example', 'current'])
+    retval = main(['--config', 'test/xprofilerc_both_example', 'current'])
 
     assert retval == 0
     assert Popen.called
@@ -78,7 +94,7 @@ def test_activate_profile(Popen):
     Popen.return_value.communicate.return_value = (b'', None)
     Popen.return_value.wait.return_value = 0
 
-    retval = main(['--config', 'test/xprofilerc_example', 'activate', 'docked'])
+    retval = main(['--config', 'test/xprofilerc_both_example', 'activate', 'docked'])
 
     assert retval == 0
     assert Popen.called
@@ -91,7 +107,7 @@ def test_activate_profile(Popen):
 
 
 def test_activate_profile_nonexistent():
-    retval = main(['--config', 'test/xprofilerc_example', 'activate', 'nonexistent'])
+    retval = main(['--config', 'test/xprofilerc_both_example', 'activate', 'nonexistent'])
 
     assert retval == 1
 
@@ -104,7 +120,7 @@ def test_activate_profile_auto(Popen):
     Popen.return_value.communicate.return_value = (xrandr_stdout, None)
     Popen.return_value.wait.return_value = 0
 
-    retval = main(['--config', 'test/xprofilerc_example', 'auto'])
+    retval = main(['--config', 'test/xprofilerc_both_example', 'auto'])
 
     assert retval == 0
     assert Popen.called
@@ -119,7 +135,7 @@ def test_activate_profile_auto__dryrun(Popen):
     Popen.return_value.communicate.return_value = (xrandr_stdout, None)
     Popen.return_value.wait.return_value = 0
 
-    retval = main(['--config', 'test/xprofilerc_example', 'auto', '--dry-run'])
+    retval = main(['--config', 'test/xprofilerc_both_example', 'auto', '--dry-run'])
 
     assert retval == 0
     assert Popen.called
@@ -134,7 +150,7 @@ def test_activate_profile_auto_nonexistent(Popen):
     Popen.return_value.communicate.return_value = (xrandr_stdout, None)
     Popen.return_value.wait.return_value = 0
 
-    retval = main(['--config', 'test/xprofilerc_example', 'auto'])
+    retval = main(['--config', 'test/xprofilerc_both_example', 'auto'])
 
     assert retval == 0
     assert Popen.called
